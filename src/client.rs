536ede843c24deec64d74aa1273e83354a30a31c
egon12 <egon.firman@gmail.com>
2025-05-18 07:58:29 +0700

    add capability write to client

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,29 @@
-use tokio::io::Error;
+use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 
 pub struct Client {
     conn: Option<TcpStream>,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    Io(std::io::Error),
+    Str(String),
+}
+
+impl Into<std::io::Error> for Error {
+    fn into(self) -> std::io::Error {
+        match self {
+            Error::Io(err) => err,
+            Error::Str(s) => std::io::Error::new(std::io::ErrorKind::Other, s),
+        }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Error {
+        Error::Io(e)
+    }
 }
 
 impl Client {
@@ -13,6 +34,15 @@ impl Client {
     pub async fn connect(&mut self, addr: String) -> Result<(), Error> {
         let conn = TcpStream::connect(addr).await?;
         self.conn = Some(conn);
+        Ok(())
+    }
+
+    pub async fn write(&mut self, data: String) -> Result<(), Error> {
+        self.conn
+            .as_mut()
+            .ok_or(Error::Str("no connection".to_string()))?
+            .write(data.as_bytes())
+            .await?;
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use tokio::io::Error;
-
 mod client;
 
 use client::Client;
+use client::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let mut c = Client::new();
     c.connect("[::1]:8083".to_string()).await?;
+    c.write("hello".to_string()).await?;
 
     Ok(())
 }
